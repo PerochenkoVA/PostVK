de46387f965389b4a73ba0fbe097667b1140f13a
Uzbek150885 <Uzbek150885@gmail.com>
2021-07-28 08:55:23 +0300

    MyVersion

---
+++ src/main/java/ru/netology/pva/CommentInfoVK.java
@@ -0,0 +1,8 @@
+package ru.netology.pva;
+
+public class CommentInfoVK {
+    private int ownerCom;       // владелец комментария
+    private int lakeCom;        // лайк комментарию
+    private String comment;     // строка комментария
+    private String reComment;   // строка ответа на комментарий
+}
---
+++ src/main/java/ru/netology/pva/PostInfoVK.java
@@ -0,0 +1,12 @@
+package ru.netology.pva;
+
+public class PostInfoVK {
+    private boolean avatar;         // аватар владельца поста
+    private  int owner;             // наименование владельца поста
+    private  int date;              // дата размещения поста
+    private String postDescription; // текст поста
+    private boolean imageVideo;     // наличие видео или фото к посту
+    private int likePost;           // количество лайков
+    private int repost;             // количество репостов
+    private int lookedPost;         // количество просмотров поста
+}
