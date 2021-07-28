d30f182637968db5319ebe05191c6c1ee5757323
Uzbek150885 <Uzbek150885@gmail.com>
2021-07-28 12:41:15 +0300

    task 1 and 2

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,31 @@
+package ru.netology.manager;
+
+public class PostManager {
+    private PostManager postManager;
+
+
+    public String getPostManager(){
+        PostManager[] posts = postManager.getPostManager();
+        //
+        return null;
+    }
+
+
+    private PostManager getWallSearch;
+        public String genWallSearch( ){
+        PostManager[] postManagers = postManager.getWallSearch;
+
+        return null;
+        }
+
+
+
+    private PostManager getWallDelete;
+        public String genWallSearch( ) {
+        PostManager[] postManagers = postManager.getWallDelete;
+
+        return null;
+        }
+}
+
+
--- src/main/java/ru/netology/pva/Post_sourceInfo.java
+++ src/main/java/ru/netology/pva/Post_sourceInfo.java
@@ -3,6 +3,7 @@ package ru.netology.pva;
 public class Post_sourceInfo {
     private String type;
     private String platform;
-    private  String data;
+    private String data;
     private String url;
+
 }
