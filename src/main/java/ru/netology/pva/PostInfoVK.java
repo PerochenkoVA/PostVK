59f9328d7e78e9daebb9d756270dd9f416623052
Uzbek150885 <Uzbek150885@gmail.com>
2021-08-11 09:44:10 +0300

    dor

--- src/main/java/ru/netology/pva/PostInfoVK.java
+++ src/main/java/ru/netology/pva/PostInfoVK.java
@@ -1,6 +1,8 @@
 package ru.netology.pva;
 
 public class PostInfoVK {
+
+
     private boolean avatar;         // аватар владельца поста
     private  int owner;             // наименование владельца поста
     private  int date;              // дата размещения поста
@@ -16,5 +18,111 @@ public class PostInfoVK {
     // boolean принимает значение: 0- доступно друзьям , 1 - доступно всем
     private int friendsOnly;       //запись была создана с опцией «Только для друзей».
 
+    private CommentInfoVK commentInfoVK;
+    private CopyringInfoVK copyringInfoVK;
+
+    public boolean isAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(boolean avatar) {
+        this.avatar = avatar;
+    }
+
+    public int getOwner() {
+        return owner;
+    }
+
+    public void setOwner(int owner) {
+        this.owner = owner;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    public String getPostDescription() {
+        return postDescription;
+    }
+
+    public void setPostDescription(String postDescription) {
+        this.postDescription = postDescription;
+    }
+
+    public boolean isImageVideo() {
+        return imageVideo;
+    }
+
+    public void setImageVideo(boolean imageVideo) {
+        this.imageVideo = imageVideo;
+    }
+
+    public int getLikePost() {
+        return likePost;
+    }
+
+    public void setLikePost(int likePost) {
+        this.likePost = likePost;
+    }
+
+    public int getRepost() {
+        return repost;
+    }
+
+    public void setRepost(int repost) {
+        this.repost = repost;
+    }
+
+    public int getLookedPost() {
+        return lookedPost;
+    }
+
+    public void setLookedPost(int lookedPost) {
+        this.lookedPost = lookedPost;
+    }
+
+    public int getReplyOwnerId() {
+        return replyOwnerId;
+    }
+
+    public void setReplyOwnerId(int replyOwnerId) {
+        this.replyOwnerId = replyOwnerId;
+    }
+
+    public int getReplyPostId() {
+        return replyPostId;
+    }
+
+    public void setReplyPostId(int replyPostId) {
+        this.replyPostId = replyPostId;
+    }
+
+    public int getFriendsOnly() {
+        return friendsOnly;
+    }
+
+    public void setFriendsOnly(int friendsOnly) {
+        this.friendsOnly = friendsOnly;
+    }
+
+    public CommentInfoVK getCommentInfoVK() {
+        return commentInfoVK;
+    }
+
+    public void setCommentInfoVK(CommentInfoVK commentInfoVK) {
+        this.commentInfoVK = commentInfoVK;
+    }
+
+    public CopyringInfoVK getCopyringInfoVK() {
+        return copyringInfoVK;
+    }
+
+    public void setCopyringInfoVK(CopyringInfoVK copyringInfoVK) {
+        this.copyringInfoVK = copyringInfoVK;
+    }
 
 }
